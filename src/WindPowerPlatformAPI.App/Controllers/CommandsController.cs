36a380411000f313383e4b6f86f893920ad71adb
Petrenko Sergii <petrenko7@yahoo.com>
2023-01-09 18:35:12 +0200

    Add project files.

---
+++ src/WindPowerPlatformAPI.App/Controllers/CommandsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WindPowerPlatformAPI.App.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CommandsController : ControllerBase
+	{
+		[HttpGet]
+		public ActionResult<IEnumerable<string>> Get()
+		{
+			return new string[] { "this", "is", "hard", "coded" };
+		}
+	}
+}
