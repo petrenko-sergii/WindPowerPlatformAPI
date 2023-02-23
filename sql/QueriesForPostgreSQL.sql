c17e0c403b23a065eedd1e634170b64ff3884747
Petrenko Sergii <petrenko7@yahoo.com>
2023-02-23 22:33:30 +0200

    Add Postman-collection file

--- sql/QueriesForPostgreSQL.sql
+++ sql/QueriesForPostgreSQL.sql
@@ -5,3 +5,11 @@ insert into "Commands" ("HowTo", "Platform", "CommandLine")
 VALUES ('Apply Migrations to DB', 'Entity Framework Core Command Line','dotnet ef database update');
 
 select * from "Commands";
+
+
+insert into "Commands" ("HowTo", "Platform", "CommandLine")
+VALUES ('Create an EF Migration', 'Entity Framework Package Manager Console','add-migration <name of
+migration>');
+
+insert into "Commands" ("HowTo", "Platform", "CommandLine")
+VALUES ('Apply Migrations to DB', 'Entity Framework Package Manager Console','update database');
