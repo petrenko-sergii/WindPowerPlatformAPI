7cdaf1503648afa6f3cc9242eb05f1fa2636b23e
Petrenko Sergii <petrenko7@yahoo.com>
2023-05-04 10:29:10 +0300

    Add db-context migration

--- src/WindPowerPlatformAPI.App/Startup.cs
+++ src/WindPowerPlatformAPI.App/Startup.cs
@@ -50,8 +50,10 @@ namespace WindPowerPlatformAPI.App
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext context)
         {
+            context.Database.Migrate();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
