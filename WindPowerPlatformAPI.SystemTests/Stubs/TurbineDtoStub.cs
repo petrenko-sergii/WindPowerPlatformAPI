e7bcb6c5af4effa9d4c1bca4ea49e234b00f2660
Petrenko Sergii <petrenko7@yahoo.com>
2023-05-23 17:36:52 +0300

    Revert "Add System tests"
    This reverts commit 02a6d84bfc4d819404b41753d0796c828a5eec4b.

--- WindPowerPlatformAPI.SystemTests/Stubs/TurbineDtoStub.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WindPowerPlatformAPI.SystemTests.Stubs
-{
-    public class TurbineDtoStub
-    {
-        public int Id { get; set; }
-        public string SerialNumber { get; set; }
-        public decimal Price { get; set; }
-    }
-}
--- WindPowerPlatformAPI.SystemTests/Usings.cs
+++
@@ -1,4 +0,0 @@
-global using Xunit;
-global using System.Net;
-global using System.Text.Json;
-global using WindPowerPlatformAPI.SystemTests.Stubs;
