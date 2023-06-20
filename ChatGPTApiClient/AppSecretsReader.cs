253a55a3dc0d6c00b8e9ee9876e4a3cd62956d72
Petrenko Sergii <petrenko7@yahoo.com>
2023-06-20 16:30:37 +0300

    Add test-app ChatGPTApiClient

---
+++ ChatGPTApiClient/AppSecretsReader.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ChatGPTApiClient
+{
+    public static class AppSecretsReader
+    {
+        public static string? ReadSection(string sectionName)
+        {
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets<Program>();
+
+            var configurationRoot = builder.Build();
+
+            return configurationRoot.GetSection(sectionName).Value;
+        }
+    }
+}
---
+++ ChatGPTApiClient/Program.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+using ChatGPTApiClient;
+
+if (args.Length > 0)
+{
+    var apiKey = AppSecretsReader.ReadSection("OpenAI_ChatGPTApiClientKey");
+
+    //todo:just for test, use IHttpClientFactory 
+    HttpClient client = new HttpClient();
+    client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+
+    var content = new StringContent("{\"model\": \"text-davinci-001\", \"prompt\": \"" + args[0] + "\",\"temperature\": 1,\"max_tokens\": 100}",
+       Encoding.UTF8, "application/json");
+
+    HttpResponseMessage response = await client.PostAsync("https://api.openai.com/v1/completions", content);
+    string responseStr = await response.Content.ReadAsStringAsync();
+
+    try
+    {
+        //todo: just for test, use typed objects / not dynamic
+        var dynamicData = JsonConvert.DeserializeObject<dynamic>(responseStr);
+        var reply = (string)dynamicData!.choices[0].text;
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        var cleanedReply = reply.Replace("\n", "").Replace("\r", "").Replace("?", "");
+
+        Console.WriteLine($"--> API response: {cleanedReply}");
+        TextCopy.ClipboardService.SetText(cleanedReply);
+        Console.ResetColor();
+    }
+    catch (Exception ex)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine( $"--> Could nort deserialize the JSON: {ex.Message}");
+        Console.ResetColor();
+    }
+
+    //Console.WriteLine(responseStr);
+}
+else
+{
+    Console.WriteLine("--> You need to provide some input");
+}
